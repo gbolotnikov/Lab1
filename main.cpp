88fa5d7fd1248c45942ce2663649a0a8de9879d2
bolotnikovgv <bolotnikovgv@mail.dev.local>
2023-03-14 12:31:40 +0300

    safaf

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 
 int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hy, world!" << std::endl;
+    std::cout << "Hy123, world!" << std::endl;
     return 0;
 }
